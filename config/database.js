042d56ef16c4b65ef220773035a6f96dbccd547a
Javel Wilson <javelinfotech@gmail.com>
2018-11-21 22:17:39 -0500

    environment variable setup

--- config/database.js
+++ config/database.js
@@ -1,10 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb://admin:UT3chS0RAT!@ds039778.mlab.com:39778/utechsorat-db'
-}
 
-// if(process.env.NODE_ENV === 'production'){
-//     module.exports = {mongoURI:
-//         'mongodb://admin:UT3chS0RAT!@ds039778.mlab.com:39778/utechsorat-db'}} else {
-//             module.exports = {mongoURI: 'mongodb://localhost:27017/utechsorat'
-//         }
-// }
+if(process.env.NODE_ENV === 'production'){
+   module.exports = require('./database_prod')
+} else {
+    module.exports = require('./database_dev')
+}
